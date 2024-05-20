48bf00471df8fae33524f8e13dbf5e04041f9f26
madankh <khadkasuman0000@gmail.com>
2024-05-20 08:16:11 +0545

    main

--- transformer/train.py
+++ transformer/train.py
@@ -1,11 +1,17 @@
 import torch
 import torch.nn as nn
 from pathlib import Path
+from torch.utils.data import Dataset, random_split, DataLoader 
+
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import WordLevel
 from tokenizers.trainers import WordLevelTrainer
 from tokenizers.pre_tokenizers import Whitespace
+
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item["translation"][lang]
 
 def get_or_build_tokenizer(config,ds , lang):
     tokenizer_Path = Path(config['tokenizer_file'].format(lang))
@@ -18,6 +24,19 @@ def get_or_build_tokenizer(config,ds , lang):
     else:
         tokenizer = Tokenizer.from_file(str(tokenizer_Path))
     return tokenizer
+
+
+def get_ds(config):
+    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config['lang_tgt']}', spilt='train')
+
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config,ds_raw,config["lang_src"])
+    tokenizer_tgt = get_or_build_tokenizer(config,ds_raw,config["lang_tgt"])
+
+    # Keep 90% data for traning and 10% for validation
+    train_ds_size = int(0.9*len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw , [train_ds_size, val_ds_size])
 
 
 
